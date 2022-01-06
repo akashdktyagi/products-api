f200d9218b055124a441cd39afeae19810e8c540
akashtyagi <akashdktyagi@gmail.com>
2022-01-06 21:32:32 +0800

    Cucumber Tests

--- src/main/java/com/yantraCloudApp/productsapi/resource/ProductRestController.java
+++ src/main/java/com/yantraCloudApp/productsapi/resource/ProductRestController.java
@@ -7,7 +7,6 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.extern.log4j.Log4j2;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
